61f8d7913a92a63d95cd039e9772d1a63610a3fd
kayque lima <kayqueferreira185@gmail.com>
2026-05-22 10:33:10 -0300

    feat: remove cards estáticos e inicia renderizaçã́o dinâmica dos cards

--- JS/Script.js
+++ JS/Script.js
@@ -1,12 +1,1 @@
 const sessao = document.querySelector ('.cards');
-
-sessao.addEventListener('click', (e) =>{
-  const card =  e.target.closest('.planos');
-  if (!card) return;
-  const todosCard = document.querySelectorAll('.planos');
-  for (let i = 0; i < todosCard.length; i++){
-  todosCard[i].classList.remove('selected');
-  }
-  card.classList.add('selected');
-
-});
